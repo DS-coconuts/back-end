0862ad3d111f0721e5dde9db51c56ab5bf376da1
pyounani <yndbsk9372@gmail.com>
2024-02-01 22:34:54 +0900

    [fix] Friend Service 수정

---
+++ src/main/java/com/example/coconuts/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.coconuts.repository;
+
+import com.example.coconuts.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
--- src/main/java/com/example/coconuts/service/FriendServiceImpl.java
+++ src/main/java/com/example/coconuts/service/FriendServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.coconuts.service;
 
 import com.example.coconuts.dto.friend.FriendListResponseDto;
+import com.example.coconuts.entity.UserEntity;
 import com.example.coconuts.repository.FriendRepository;
+import com.example.coconuts.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,14 @@ public class FriendServiceImpl implements FriendService{
 
     private final FriendRepository friendRepository;
 
+    private final UserRepository userRepository;
+
     @Override
     public List<FriendListResponseDto> getFriendList(Integer userId) {
-        friendRepository.findByFromUserId(userId);
+        UserEntity user = userRepository.findById(userId).orElse(null);
+        if (user != null) {
+            friendRepository.findByFromUser(user);
+        }
         return null;
     }
 }
