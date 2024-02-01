be6ed8f851e26aed040b979f7479d1b268cc694b
pyounani <yndbsk9372@gmail.com>
2024-02-01 19:30:49 +0900

    [fix] 친구 관계 정의를 위한 엔티티 수정

--- src/main/java/com/example/coconuts/service/FriendServiceImpl.java
+++ src/main/java/com/example/coconuts/service/FriendServiceImpl.java
@@ -1,4 +1,21 @@
 package com.example.coconuts.service;
 
+import com.example.coconuts.dto.friend.FriendListResponseDto;
+import com.example.coconuts.repository.FriendRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class FriendServiceImpl implements FriendService{
+
+    private final FriendRepository friendRepository;
+
+    @Override
+    public List<FriendListResponseDto> getFriendList(Integer userId) {
+        friendRepository.findByFromUserId(userId);
+        return null;
+    }
 }
