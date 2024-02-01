7a769e1e62aadee9d1209dafaa19b4ad393255be
pyounani <yndbsk9372@gmail.com>
2024-02-01 10:48:12 +0900

    [init] 프로젝트 초기 설정

---
+++ src/main/java/com/example/coconuts/CoconutsApplication.java
@@ -0,0 +1,13 @@
+package com.example.coconuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoconutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoconutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coconuts/CoconutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coconuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoconutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
